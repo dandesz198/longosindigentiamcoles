5912f1c4862f561a8f156555289ff3cc518cd702
Daniel <dandesz198@gmail.com>
2018-11-09 12:16:56 +0100

    Basic backend setup

---
+++ backend/models/user.js
@@ -0,0 +1,77 @@
+const db = require('./db')
+const { generateSalt, hash } = require('../utils/hash')
+const { throw400, checkDuplicates } = require('../utils/errors')
+
+const collectionName = 'users'
+
+async function search () {
+  return db
+    .collection(collectionName)
+    .find()
+    .sort({ name: 1 })
+    .project({ email: 1, name: 1 })
+    .toArray()
+}
+
+async function login ({ email, password }) {
+  const user = await db.collection(collectionName).findOne({ email })
+
+  if (!user) {
+    throw400({
+      email: 'No matching email found'
+    })
+  }
+  if (user.password !== hash(password, user.salt)) {
+    throw400({
+      password: 'Incorrect password'
+    })
+  }
+
+  // do not include the password and salt in the response
+  user.password = undefined
+  user.salt = undefined
+  return user
+}
+
+async function register ({ email, password, name }) {
+  // hash the user password before storing it
+  const salt = generateSalt()
+  password = hash(password, salt)
+
+  const user = { email, name, password, salt }
+  await db
+    .collection(collectionName)
+    .insertOne(user)
+    .catch(err =>
+      checkDuplicates(err, {
+        email: 'This email is already taken'
+      })
+    )
+
+  // do not include the password and salt in the response
+  user.password = undefined
+  user.salt = undefined
+  return user
+}
+
+async function update (id, data) {
+  await db
+    .collection(collectionName)
+    .updateOne({ _id: id }, { $set: data })
+    .catch(err =>
+      checkDuplicates(err, {
+        email: 'This email is already taken'
+      })
+    )
+  return data
+}
+
+module.exports = {
+  get raw () {
+    return db.collection(collectionName)
+  },
+  search,
+  login,
+  register,
+  update
+}
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,55 @@
+require("dotenv").config();
+const logger = require("../models/logger");
+// these need a one off sync init
+require("../models/passport");
+// these require async init and cleanup
+const db = require("../models/db");
+const server = require("./server");
+
+// graceful start
+init();
+
+// graceful shutdown
+const stopSignals = ["SIGINT", "SIGTERM"];
+stopSignals.forEach(signal => process.once(signal, stop));
+
+// do not init the server if a crucial component can not start up
+async function init() {
+  try {
+    await db.init();
+    await server.init();
+  } catch (err) {
+    logger.error(`Couldn't init the app: ${err}`);
+    // exit code for fatal exception
+    process.exit(1);
+  }
+}
+
+async function stop() {
+  // start with a normal exit code
+  let exitCode = 0;
+  try {
+    await server.close();
+  } catch (err) {
+    logger.error(`Failed to close the server: ${err}`);
+    exitCode = 1;
+  }
+
+  try {
+    await db.close();
+  } catch (err) {
+    logger.error(`Failed to close the db: ${err}`);
+    exitCode = 1;
+  }
+  process.exit(exitCode);
+}
+
+module.exports = {
+  init,
+  stop
+};
+
+/*
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -85,3 +137,4 @@ app.use("/api", router);
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+*/
