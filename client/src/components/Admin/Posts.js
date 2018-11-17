e555f688cf1f2331f38d2312a6b37f717445b25f
Lesti Dániel <daniellesti73@gmail.com>
2018-11-17 13:38:27 +0100

    Admin sidenav

---
+++ client/src/components/Admin/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Posts = ()=>{
+    return(
+        <h1>
+            Itt vannak posztok, amiket nem látni de ittvannak
+        </h1>
+    )
+};
+
+
+export default Posts;
---
+++ client/src/components/Admin/Users.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Users = ()=>{
+    return(
+        <h1>Ezmeg nemtom mé users úgy hogy nincsenek is user fiókok.. </h1>
+    )
+}
+
+export default Users;
