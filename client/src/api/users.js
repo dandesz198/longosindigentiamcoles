2fea1d2a1b2e08510f5ae7e0861841468e853180
Daniel <dandesz198@gmail.com>
2018-11-23 11:58:47 +0100

    Actually working user API endpoints

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,28 +1,30 @@
-import { storage } from 'react-stax'
-import api from './api'
+import { storage } from "react-stax";
+import api from "./api";
 
 function processUser(data) {
-  api.defaults.headers.Authorization = `Bearer ${data.token}`
-  storage.token = data.token
-  return data.user
+  api.defaults.headers.Authorization = `Bearer ${data.token}`;
+  storage.token = data.token;
+  return data.user;
 }
 
-export async function login(loginData) {
-  const { data } = await api.post('/login', loginData)
-  return processUser(data)
+export async function login({ email, password }) {
+  const { data } = await api.post(`/login?email=${email}&password=${password}`);
+  return processUser(data);
 }
 
-export async function register(registerData) {
-  const { data } = await api.post('/register', registerData)
-  return processUser(data)
+export async function register({ email, password, name, role }) {
+  const { data } = await api.post(
+    `/register?email=${email}&password=${password}&name=${name}&role=${role}`
+  );
+  return processUser(data);
 }
 
 export function logout() {
-  delete api.defaults.headers.Authorization
-  delete storage.token
+  delete api.defaults.headers.Authorization;
+  delete storage.token;
 }
 
 export async function list() {
-  const { data } = await api.get('/users')
-  return data
+  const { data } = await api.get("/users");
+  return data;
 }
