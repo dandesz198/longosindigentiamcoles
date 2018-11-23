2d11ffec1894d3b9c2796f49139b8cb3ff584c46
Daniel <dandesz198@gmail.com>
2018-11-23 12:23:51 +0100

    hg

---
+++ client/src/api/article.js
@@ -0,0 +1,8 @@
+import api from './api'
+
+// export async function removeFeature({ }) {
+//   const { data } = await api.delete(
+//     `/stories/${storyId}/slides/${slideId}/features/${featureId}`
+//   )
+//   return data
+// }
