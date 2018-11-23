7927b9363e7485cea3f34996982197b49b1c842e
Daniel <dandesz198@gmail.com>
2018-11-23 11:31:04 +0100

    Routes fix

--- backend/web/routes/index.js
+++ backend/web/routes/index.js
@@ -10,17 +10,17 @@ const privateRouter = new Router()
 privateRouter.use(passport.authenticate('jwt', { session: false }))
 
 // user endpoints
-publicRouter.post('/register', catchAsyncErrors(users.register))
-publicRouter.post('/login', catchAsyncErrors(users.login))
-publicRouter.get('/all', catchAsyncErrors(users.all))
-privateRouter.put('/me', catchAsyncErrors(users.update))
-privateRouter.delete('/delete/:id', catchAsyncErrors(users.delete))
+publicRouter.post('/user/register', catchAsyncErrors(users.register))
+publicRouter.post('/user/login', catchAsyncErrors(users.login))
+publicRouter.get('/user/all', catchAsyncErrors(users.all))
+privateRouter.put('/user/me', catchAsyncErrors(users.update))
+privateRouter.delete('/user/delete/:id', catchAsyncErrors(users.delete))
 
 // article endpoints
-privateRouter.post('/create', catchAsyncErrors(articles.create))
-publicRouter.get('/get', catchAsyncErrors(articles.getAll))
-publicRouter.get('/get:id', catchAsyncErrors(articles.getById))
-privateRouter.delete('/delete/:id', catchAsyncErrors(articles.delete))
+privateRouter.post('/article/create', catchAsyncErrors(articles.create))
+publicRouter.get('/article/get', catchAsyncErrors(articles.getAll))
+publicRouter.get('/article/get:id', catchAsyncErrors(articles.getById))
+privateRouter.delete('/article/delete/:id', catchAsyncErrors(articles.delete))
 
 module.exports = {
   public: publicRouter,
