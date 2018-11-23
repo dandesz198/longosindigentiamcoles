3f2be102aaac74d2b6238e21ed5701433ffcc8ae
Daniel <dandesz198@gmail.com>
2018-11-23 11:48:19 +0100

    axios interceptor

---
+++ client/src/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { storage } from 'react-stax'
+
+const api = axios.create({
+  baseURL: 'http://localhost:3005/api',
+  timeout: 10000
+})
+
+if (storage.token) {
+  api.defaults.headers.Authorization = `Bearer ${storage.token}`
+}
+
+api.interceptors.request.use(config => {
+  config.intercepted = true
+  return config
+})
+
+api.interceptors.response.use(
+  resp => {
+    return resp
+  },
+  err => {
+    // TODO: also display a notification here
+    // if there is err.response.data.msg!
+    throw err
+  }
+)
+
+export default api
