8db9317f4b3cadcf12a360b86c44e45864caacf1
Daniel <dandesz198@gmail.com>
2018-11-30 10:49:35 +0100

    error handling

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,31 +1,32 @@
-import axios from 'axios'
-import { storage } from 'react-stax'
-import modalStore from '../stores/modalStore';
+import axios from "axios";
+import { storage } from "react-stax";
+import modalStore from "../stores/modalStore";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3005/api',
+  baseURL: "http://localhost:3005/api",
   timeout: 10000
-})
+});
 
 if (storage.token) {
-  api.defaults.headers.Authorization = `Bearer ${storage.token}`
+  api.defaults.headers.Authorization = `Bearer ${storage.token}`;
 }
 
 api.interceptors.request.use(config => {
-  config.intercepted = true
-  return config
-})
+  config.intercepted = true;
+  return config;
+});
 
 api.interceptors.response.use(
   resp => {
-    return resp
+    return resp;
   },
   err => {
+    const { msg } = err.response.data;
     // TODO: also display a notification here
     // if there is err.response.data.msg!
-    modalStore.show(err.response.data.msg)
-    throw err
+    modalStore.show(msg || err.message);
+    throw err;
   }
-)
+);
 
-export default api
+export default api;
